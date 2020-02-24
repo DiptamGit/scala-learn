bc445e4e185f57e9724e5e1f06b5d65c836915a7
Diptam Sarkar <diptamsarkar@in.ibm.com>
2020-02-24 15:04:28 -0600

    Initial commit

---
+++ src/main/scala/ScalaFirst.scala
@@ -0,0 +1,18 @@
+object ScalaFirst {
+
+  def main(args: Array[String]): Unit = {
+
+    //While Loop
+    var i = 0;
+    while (i <= 10){
+      print(i+" ");
+      i += 1;
+    }
+  }
+
+  //for loop
+  for(i <- 1 to 10){
+    print(i);
+  }
+
+}
