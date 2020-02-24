b660851c6ec3efafc711807822bab7c3aa479b60
Diptam Sarkar <diptamsarkar@gmail.com>
2020-02-24 15:10:21 -0600

    for loop for String

--- src/main/scala/ScalaFirst.scala
+++ src/main/scala/ScalaFirst.scala
@@ -5,14 +5,20 @@ object ScalaFirst {
     //While Loop
     var i = 0;
     while (i <= 10){
-      print(i+" ");
+      println(i+" ");
       i += 1;
     }
   }
 
   //for loop
   for(i <- 1 to 10){
-    print(i);
+    println(i);
+  }
+
+  //loop through String
+  var alphabates = "abcdefghijklmnopqrstuvwxyz"
+  for (i <- 0 until alphabates.length){
+    println(alphabates(i));
   }
 
 }
